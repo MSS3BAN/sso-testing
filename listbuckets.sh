ee3551afce4c50b7559c03a790c2b11afcacc884
Seshasai <135961125+MSS3BAN@users.noreply.github.com>
2023-09-28 13:10:45 +0530

    Update listbuckets.sh

--- listbuckets.sh
+++ listbuckets.sh
@@ -1,3 +1,22 @@
-#!/bin/bash
+name: "sso-testing"
 
-aws s3 ls
+ 
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:   
+
+      - name: Check out repository
+        uses: actions/checkout@v3
+
+      - name: Configure AWS SSO Profile
+        uses: aws-actions/configure-aws-credentials@v1
+        with:
+          AliceAdmin-598405220016
+          aws-region: eu-central-1
+
+      - name: list s3
+        run: aws s3 ls
